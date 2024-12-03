e26399cae8e406e2ca1d35034043d0570b5cf92a
Neal Fennimore <hi@neal.codes>
2024-12-03 12:34:16 -0500

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,25 @@
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+mkShell {
+  buildInputs = [
+    python312Packages.virtualenv
+  ];
+
+  shellHook = ''
+    [[ ! -d .venv ]] && python -m venv .venv
+    source .venv/bin/activate
+    if [[ -f requirements.txt ]]; then
+      pip install -r requirements.txt --require-virtualenv
+    fi
+  '';
+
+  packages = [
+    (python312.withPackages
+      (
+        pp: [
+          pp.fastapi
+          pp.jupyter
+        ]
+      ))
+  ];
+}
