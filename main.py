f35422cbdb4e8c381c1e8d3a4a4b4534ac8eb254
Neal Fennimore <hi@neal.codes>
2024-12-03 13:22:05 -0500

    Add in element validation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from fastapi import FastAPI, HTTPException
 
@@ -22,8 +23,13 @@ async def read_framework() -> Framework:
     raise HTTPException(status_code=404, detail="Framework not found")
 
 
+element_re = re.compile(r"^\d{2}\.\d{2}(\.\d{2})?$")
+
+
 @app.get("/sp_800_171_3_0_0/elements/{element}")
 async def read_element(element: str) -> Elements:
+    if not element_re.match(element):
+        raise HTTPException(status_code=400, detail="Invalid element identifier")
     file_path = os.path.join(DATA_FOLDER, "elements", f"{element}.json")
     if os.path.exists(file_path):
         with open(file_path, "r") as file:
