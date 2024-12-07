3a7249e676233faaa013a2ab568396894d83a9ef
Neal Fennimore <hi@neal.codes>
2024-12-07 10:45:27 -0500

    Update to allow for closing of menu on escape or click outside of nav

--- client/src/app/components/navigation.tsx
+++ client/src/app/components/navigation.tsx
@@ -1,11 +1,40 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { ClearDB } from "./clear_db";
 import { Export, Import } from "./export_import";
 import { Markdown } from "./markdown";
 
 export const Navigation = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef<HTMLElement>(null);
+    const onKeyDown = useMemo(
+        () => (event: KeyboardEvent) => {
+            if (event.key === "Escape") {
+                setIsOpen(false);
+            }
+        },
+        []
+    );
+    const onClick = useMemo(
+        () => (event: MouseEvent) => {
+            if (menuRef.current?.contains(event.target as Node)) {
+                return;
+            }
+            setIsOpen(false);
+        },
+        []
+    );
+
+    useEffect(() => {
+        if (isOpen) {
+            document.addEventListener("keydown", onKeyDown);
+            document.addEventListener("click", onClick);
+        } else {
+            document.removeEventListener("keydown", onKeyDown);
+            document.removeEventListener("click", onClick);
+        }
+    }, [isOpen]);
+
     return (
         <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -42,21 +71,6 @@ export const Navigation = () => {
                                     <rect y="9" width="30" height="3" />
                                     <rect y="18" width="30" height="3" />
                                 </svg>
-                                {/* <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24"
-                                    width="30"
-                                    height="30"
-                                    fill="currentColor"
-                                    aria-hidden="true"
-                                    data-slot="icon"
-                                >
-                                    <path
-                                        fillRule="evenodd"
-                                        d="m17.653 10.35 2.056.336a.347.347 0 0 1 .291.343v1.913a.348.348 0 0 1-.288.342l-2.058.362a5.82 5.82 0 0 1-.49 1.185l1.217 1.69c.1.138.084.328-.036.45l-1.353 1.352a.347.347 0 0 1-.445.04l-1.707-1.198c-.376.21-.776.377-1.193.5l-.363 2.048a.347.347 0 0 1-.343.287h-1.913a.347.347 0 0 1-.343-.29l-.34-2.039a5.87 5.87 0 0 1-1.197-.495l-1.694 1.186a.348.348 0 0 1-.446-.038L5.655 16.97a.349.349 0 0 1-.036-.448l1.197-1.674a5.865 5.865 0 0 1-.5-1.204l-2.029-.36A.348.348 0 0 1 4 12.942v-1.913c0-.17.123-.315.29-.343l2.03-.338c.121-.42.289-.822.5-1.203L5.636 7.453a.347.347 0 0 1 .039-.445l1.352-1.352c.12-.12.31-.136.448-.037l1.68 1.2a5.86 5.86 0 0 1 1.196-.491l.333-2.036A.348.348 0 0 1 11.028 4h1.913c.17 0 .314.122.343.288l.359 2.047c.415.121.815.287 1.193.497l1.685-1.211a.348.348 0 0 1 .45.036l1.352 1.352c.12.12.136.308.039.446l-1.2 1.71c.206.376.37.772.49 1.185ZM9.565 12a2.435 2.435 0 1 0 4.87 0 2.435 2.435 0 0 0-4.87 0Z"
-                                        clipRule="evenodd"
-                                    />
-                                </svg> */}
                             </button>
                         </div>
                         {isOpen && (
@@ -68,6 +82,7 @@ export const Navigation = () => {
                                 tabIndex={-1}
                                 onSubmit={() => setIsOpen(false)}
                                 onKeyUp={() => setIsOpen(false)}
+                                ref={menuRef}
                             >
                                 <div className="py-1" role="none">
                                     <Markdown />
