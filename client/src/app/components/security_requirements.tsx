226d32a7a18f8d8fa6b041c46fca6bfa43444902
Neal Fennimore <hi@neal.codes>
2024-12-06 19:00:23 -0500

    Remove max length

--- client/src/app/components/security_requirements.tsx
+++ client/src/app/components/security_requirements.tsx
@@ -92,7 +92,6 @@ const SecurityRequirementNote = ({
     isPending,
 }: SecurityRequirementProps) => {
     const key = `${securityRequirement.subSubRequirement}.description`;
-    const maxLength = 2048;
     return (
         <div className="flex flex-col grow">
             <label
@@ -105,8 +104,6 @@ const SecurityRequirementNote = ({
                 name={key}
                 id={key}
                 rows={5}
-                maxLength={maxLength}
-                placeholder={`[Max ${maxLength} chars]`}
                 className="grow w-full rounded-md border border-input bg-transparent px-3 py-3 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
                 disabled={isPending}
                 defaultValue={initialState[key]}
