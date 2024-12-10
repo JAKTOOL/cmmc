d5eeec3143e0829c01e9dc61aaad7b4d271327d5
Neal Fennimore <hi@neal.codes>
2024-12-09 21:35:16 -0500

    Update to scale output to retain height calculation

--- client/src/app/components/security_requirements.tsx
+++ client/src/app/components/security_requirements.tsx
@@ -196,7 +196,7 @@ const SecurityRequirementNote = ({
                     className={`min-h-32 relative z-10 md-output w-full rounded-md border border-input bg-white px-3 py-3 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm ${
                         showOutput && textareaRef?.current?.value
                             ? ""
-                            : "hidden"
+                            : "scale-x-0"
                     }`}
                 ></div>
             </div>
