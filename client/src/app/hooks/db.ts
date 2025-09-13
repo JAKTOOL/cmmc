1f322cd2e6a46618b070f9abb07dc9088217ad3e
Neal Fennimore <neal@neal.codes>
2025-09-12 23:15:09 -0400

    Add in db hooks

--- client/src/app/hooks/db.ts
+++ client/src/app/hooks/db.ts
@@ -1,6 +1,6 @@
 "use client";
-import { IDB, IDBRequirement } from "@/app/db";
-import { useEffect, useState } from "react";
+import { IDB, IDBRequirement, IDBSecurityRequirement } from "@/app/db";
+import { useEffect, useReducer, useState } from "react";
 
 export const useDBRequirements = () => {
     const [dbRequirements, setDBRequirements] = useState(
@@ -16,4 +16,31 @@ export const useDBRequirements = () => {
     }, []);
 
     return dbRequirements;
+};
+export const useDBSecurityRequirements = () => {
+    const [dbRequirements, setDBRequirements] = useState(
+        undefined as IDBSecurityRequirement[] | undefined
+    );
+
+    useEffect(() => {
+        async function fetchInitialState() {
+            const reqs = await IDB.securityRequirements.getAll();
+            setDBRequirements(reqs);
+        }
+        fetchInitialState();
+    }, []);
+
+    return dbRequirements;
+};
+
+const initialState = { id: crypto.randomUUID() };
+export function transactionReducer(state, action) {
+    switch (action.type) {
+        default:
+            return { id: crypto.randomUUID() };
+    }
+}
+
+export const useTransaction = () => {
+    return useReducer(transactionReducer, initialState);
 };
