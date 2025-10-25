1c58fc14fe4f220aab41519e9ea1df6b2fe0af51
nealfennimore <nealfennimore@users.noreply.github.com>
2025-10-25 17:31:31 +0000

    deploy: 25dd36861f96e3fc599a66d71f01c5318d0e9167

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js');
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register(
+    `/sw.js?${process.env.NEXT_PUBLIC_FRAMEWORK_VERSION}`
+  );
 }
