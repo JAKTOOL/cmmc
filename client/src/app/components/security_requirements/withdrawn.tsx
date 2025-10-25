b008fa6f7c047db58be4dff48fe093dcf555867e
Neal Fennimore <neal@neal.codes>
2025-10-25 10:34:55 -0400

    Add in warning for withdrawn controls

--- client/src/app/components/security_requirements/withdrawn.tsx
+++ client/src/app/components/security_requirements/withdrawn.tsx
@@ -51,10 +51,41 @@ export const WithdrawnSecurityRequirement = ({
     return (
         <>
             <Breadcrumbs requirementId={requirementId} />
+
             <h3 className="text-3xl mt-6">
                 Security Requirements for {requirement.requirement}
                 <StatusState statuses={statuses} />
             </h3>
+
+            <div
+                id="alert-additional-content-5"
+                className="p-4 border border-yellow-100 text-yellow-800  rounded-lg bg-yellow-50 "
+                role="alert"
+            >
+                <div className="flex items-center">
+                    <svg
+                        className="shrink-0 w-4 h-4 me-2"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                    </svg>
+                    <span className="sr-only">Info</span>
+                    <h3 className="text-lg font-sm text-gray-800 ">
+                        Withdrawn
+                    </h3>
+                </div>
+                <div className="mt-2 mb-4 text-xs text-gray-800">
+                    <p className="mb-2">
+                        This security requirement has been withdrawn as of
+                        NIST-SP 800-171 revision 3, but is retained as CMMC is
+                        using revision 2.
+                    </p>
+                </div>
+            </div>
+
             <p
                 className="text-base discussion"
                 dangerouslySetInnerHTML={{
@@ -63,6 +94,7 @@ export const WithdrawnSecurityRequirement = ({
                             ?.text || "",
                 }}
             ></p>
+
             <aside className="flex flex-wrap justify-between items-center w-full mx-auto">
                 <div className="flex mb-4 sm:mb-1">
                     <a
