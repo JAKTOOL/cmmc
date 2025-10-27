7549b840d920ecfbcf07bd4d35028e220b72f8e5
Neal Fennimore <neal@neal.codes>
2025-10-27 14:11:40 -0400

    Fix negative numbers not going to 2 decimals

--- client/src/app/utils/number.ts
+++ client/src/app/utils/number.ts
@@ -1,6 +1,6 @@
 export const renderNumber = (i: number | undefined) => {
     const n: number | string = i ?? 0;
-    if (n % 1 > 0) {
+    if (Math.abs(n) % 1 > 0) {
         return n.toFixed(2);
     }
     return n;
